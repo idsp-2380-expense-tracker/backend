0794e403ccaa785a5a4ac23678732c67e0277b09
Kyle <kyou4@my.bcit.ca>
2025-04-29 06:20:51 -0700

    initial commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { Request, Response } from "express";
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
