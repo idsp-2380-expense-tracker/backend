6e441073d167da1b21ef2018da407a191e795043
Kyle <kyou4@my.bcit.ca>
2025-05-01 12:22:56 -0700

    test auth

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -16,19 +16,19 @@ router.post(
 
     try {
       const user = await clerkClient.users.getUser(userId!);
-
+      console.log(user);
       const userData: ClerkUser = {
         userId: userId!,
         firstName: user.firstName!,
         lastName: user.lastName!,
         email: user.emailAddresses[0].emailAddress,
       };
-
+      console.log(userData);
       const existingUser = await checkUserInDb(userId!);
       if (!existingUser) {
         await insertUserToDb(userData);
       }
-
+      console.log(existingUser);
       res.json({ success: true });
     } catch {
       res
