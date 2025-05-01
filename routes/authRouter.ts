d1eef4c92401a56cc06b1b4b1b65c397deb59836
Kyle <kyou4@my.bcit.ca>
2025-05-01 11:19:31 -0700

    test auth

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -6,10 +6,22 @@ const router = Router();
 
 router.use("/user", userRouter);
 
-router.get("/checkUser", (req: Request, res: Response) => {
-  const userData = req.auth;
-  console.log(userData);
-  res.json(userData);
+router.get("/checkUser", requireAuth(), async (req: Request, res: Response) => {
+  const { userId } = req.auth;
+
+  if (!userId) return;
+
+  try {
+    const user = await clerkClient.users.getUser(userId);
+
+    const email = user.emailAddresses[0].emailAddress;
+    const firstName = user.firstName;
+    const lastName = user.lastName;
+
+    res.json({ userId, email, firstName, lastName });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 export default router;
