8db2764a87bcd3084596ff5e146cd868d3d2b665
Kyle <kyou4@my.bcit.ca>
2025-05-08 12:49:15 -0700

    test budget

--- src/areas/Budget/budgetRouter.ts
+++ src/areas/Budget/budgetRouter.ts
@@ -1,7 +1,8 @@
+import { requireAuth } from "@clerk/express";
 import { Router } from "express";
 const router = Router();
 
-router.post("/", (req, res) => {
+router.post("/", requireAuth(), (req, res) => {
   console.log(req.body);
 });
 export default router;
